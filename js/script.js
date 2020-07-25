7deeec1ad648763ec0c20466482db8a3bba4cbe2
MizanUrp11 <mrn1105009@gmail.com>
2020-07-25 12:22:38 +0600

    complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-    
+  $("#stamp1, #stamp2,#stamp3").on("click", function () {
+    var srcGet = $(this).attr("src");
+    $("#preview").attr("src", srcGet);
+  });
 });
